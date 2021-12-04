13eabdf47fd7ec2f67c094f5d1a1c35921ba7338
Tomas Hanicinec <tomas.hanicinec@gmail.com>
2021-12-04 14:39:44 +0100

    Day 04

---
+++ day_04.py
@@ -0,0 +1,59 @@
+import utils
+
+BOARD_SIZE = 5
+
+
+def main():
+    # lines = utils.read_strings('inputs/t.txt')
+    lines = utils.read_strings('inputs/day_04.txt')
+    draw = map(int, lines[0].split(','))
+
+    i = 2  # skip the first blank line
+    boards = []
+    while i < len(lines):
+        boards.append(Board(lines[i:(i + BOARD_SIZE)]))
+        i += BOARD_SIZE + 1  # skip the blank line before the next board
+
+    winning_boards_count = 0
+    for number in draw:
+        for board in boards:
+            if board.bingo:
+                continue
+            board.check(number)
+            if board.bingo:
+                winning_boards_count += 1
+                if winning_boards_count == 1:
+                    print(f'First winning board score: {board.score * number}')
+                if winning_boards_count == len(boards):
+                    print(f'Last winning board score: {board.score * number}')
+                    return
+
+
+class Board:
+    bingo = False
+    score = 0
+
+    def __init__(self, board_lines):
+        self.__row_sums = [0] * BOARD_SIZE
+        self.__col_sums = [0] * BOARD_SIZE
+        self.__board = []
+        for i in range(BOARD_SIZE):
+            self.__board.append([])
+            for val in board_lines[i].split():
+                self.score += int(val)
+                self.__board[i].append(int(val))
+
+    def check(self, number):
+        for i in range(BOARD_SIZE):
+            for j in range(BOARD_SIZE):
+                if self.__board[i][j] == number:
+                    self.score -= number
+                    self.__row_sums[i] += 1
+                    self.__col_sums[j] += 1
+                    if self.__row_sums[i] == BOARD_SIZE or self.__col_sums[j] == BOARD_SIZE:
+                        self.bingo = True
+                    return  # let's say numbers in the single board do not repeat
+
+
+if __name__ == '__main__':
+    main()
--- test.py
+++ test.py
@@ -3,11 +3,13 @@ from contextlib import redirect_stdout
 from day_01 import main as main_01
 from day_02 import main as main_02
 from day_03 import main as main_03
+from day_04 import main as main_04
 
 expected_outputs = (
     '1752 measurements are larger than the previous measurement\n1781 sums that are larger than the previous sum\n',
     'Submarine position (no aim): 1893605\nSubmarine position (with aim): 2120734350\n',
-    'Submarine power consumption: 3901196\nSubmarine life support rating: 4412188\n'
+    'Submarine power consumption: 3901196\nSubmarine life support rating: 4412188\n',
+    'First winning board score: 74320\nLast winning board score: 17884\n'
 )
 
 
